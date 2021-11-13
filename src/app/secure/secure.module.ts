e142c7901b16b4e9322c512e7ad3f2e44623a422
OnoprienkoArtem <o.artem.e@gmail.com>
2021-11-13 23:55:13 +0200

    create basic components for secure and public modules

--- src/app/secure/secure.module.ts
+++ src/app/secure/secure.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { NavComponent } from './nav/nav.component';
+import { SecureComponent } from './secure.component';
 
 
 
@@ -9,9 +10,11 @@ import { NavComponent } from './nav/nav.component';
   declarations: [
     NavComponent,
     MenuComponent,
+    SecureComponent,
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [SecureComponent]
 })
 export class SecureModule { }
