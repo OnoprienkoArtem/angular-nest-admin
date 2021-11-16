c39f03b5e34771f9f927ac81b449bf21ffbf98af
OnoprienkoArtem <o.artem.e@gmail.com>
2021-11-16 23:48:44 +0200

    two way data binding for each field of register

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
+  passwordConfirm = '';  
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit(): void {
+
   }
 
 }
