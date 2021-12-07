498ee308be8a64f1f9da55755bd27484f59ddf6d
OnoprienkoArtem <o.artem.e@gmail.com>
2021-12-07 23:31:22 +0200

    add dashbourd routh and add router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: SecureComponent,
     children: [
       {
-        path: '',
+        path: 'dashboard',
         component: DashboardComponent,
       },
       {
