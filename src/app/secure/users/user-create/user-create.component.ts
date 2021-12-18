1c595f4bb05169e7dc4eed1448b00d9281d7f719
OnoprienkoArtem <o.artem.e@gmail.com>
2021-12-18 23:01:51 +0200

    add style and role id for ieach option

--- src/app/secure/users/user-create/user-create.component.ts
+++ src/app/secure/users/user-create/user-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Role } from 'src/app/interfaces/role';
 import { RoleService } from 'src/app/services/role.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -15,6 +16,7 @@ export class UserCreateComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private roleService: RoleService,  
+    private userService: UserService,
   ) { }
 
   ngOnInit(): void {
@@ -25,8 +27,12 @@ export class UserCreateComponent implements OnInit {
       role_id: '',
     });
 
-    this.roleService.all().subscribe((res: any): any => {
-      this.roles = res.data;
+    this.roleService.all().subscribe((roles: Role[]): void => {
+      this.roles = roles;
     });
+  }
+
+  submit(): void {
+
   }
 }
