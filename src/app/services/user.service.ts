32c84b52b26a6d8a4c98c2c0121abad169a1c738
OnoprienkoArtem <o.artem.e@gmail.com>
2021-12-22 23:28:21 +0200

    create abstract service restservices with all api method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,34 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { User } from '../interfaces/user';
+import { RestService } from './rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService extends RestService {
   endpoint: string = `${environment.api}/users`;
-
-  constructor(private http: HttpClient) { }
-
-  all(page: number): Observable<User[]> {
-    return this.http.get<User[]>(`${this.endpoint}?page=${page}`);
-  }
-
-  create(data: User): Observable<User> {
-    return this.http.post<User>(this.endpoint, data);
-  }
-
-  getUser(id: number): Observable<User> {
-    return this.http.get<User>(`${this.endpoint}/${id}`);
-  }
-
-  update(id: number, data: User): Observable<User> {
-    return this.http.put<User>(`${this.endpoint}/${id}`, data);
-  }
-
-  delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.endpoint}/${id}`);
-  }
 }
