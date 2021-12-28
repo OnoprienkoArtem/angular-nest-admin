157db5ebb85d6b90a6f7fdc3af30e6dd3fa7441b
OnoprienkoArtem <o.artem.e@gmail.com>
2021-12-28 23:46:22 +0200

    done permission part

--- src/app/secure/roles/role-create/role-create.component.ts
+++ src/app/secure/roles/role-create/role-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Permission } from 'src/app/interfaces/permission';
+import { PermissionService } from 'src/app/services/permission.service';
 
 @Component({
   selector: 'app-role-create',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role-create.component.scss']
 })
 export class RoleCreateComponent implements OnInit {
+  form!: FormGroup;
+  permissions: Permission[] = [];
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private permissionService: PermissionService,
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ''
+    });
+
+    this.permissionService.all().subscribe(permissions => {
+      this.permissions = permissions;
+    });
   }
 
 }
