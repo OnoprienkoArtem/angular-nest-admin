a0b7f90ce3dd1b83a4daac5413253b4db4671b87
OnoprienkoArtem <o.artem.e@gmail.com>
2022-01-15 23:52:13 +0200

    implement logic creating product

--- src/app/secure/products/product-create/product-create.component.ts
+++ src/app/secure/products/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-create',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.scss']
 })
 export class ProductCreateComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: '',
+      description: '',
+      image: '',
+      price: '',
+    });
   }
 
+  submit(): void {
+    this.productService.create(this.form.getRawValue()).subscribe(() => {
+      this.router.navigate(['/products']);
+    });
+  }
 }
